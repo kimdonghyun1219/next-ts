eba0f33f455b0003a3cec86a841601de35b2e87c
hyun <happy951219@naver.com>
2023-09-27 10:28:02 +0900

    chore: change yarn berry(#4)

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  webpack(config) {
+    config.module.rules.push(
+      // Convert all other *.svg imports to React Components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },  // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    );
+    return config;
+  }
 };
 
 module.exports = nextConfig;
